1da7a522314b8b4e9f626f324eeb23e7476c3852
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-11-26 09:38:18 +0300

    https cookie

--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,7 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      // port: 25060,
-      // TODO uncomment above
+      port: 25060,
       ssl: true,
   }
 };
